38f72a5b2c68e14439293655a749836aa0045b19
Janani141204 <jananikannan004@gmail.com>
2025-06-02 09:39:36 +0530

    Update repea_count.java

--- repea_count.java
+++ repea_count.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    static long repeat(String s, long n) {
-        long count = 0;
+    static int repeat(String s, long n) {
+       int count = 0;
         for (char c : s.toCharArray()) {
             if (c == 'a') {
                 count++;
             }
         }
-        long full = n / s.length();
-        long total = full * count;
-        long rem = n % s.length();
+        int full = n / s.length();
+        int total = full * count;
+        int rem = n % s.length();
         for (int i = 0; i < rem; i++) {
             if (s.charAt(i) == 'a') {
                 total++;
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        long n = scanner.nextLong();
+        int n = scanner.nextInt();
         System.out.println(repeat(s, n));
         scanner.close();
     }
